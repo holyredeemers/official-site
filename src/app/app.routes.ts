51366dfb30a4e065e1e7dc56ab20b79cc322358f
Kingswin <kingswin5549@gmail.com>
2025-02-24 22:59:13 +0530

    Initial commit of Holy Redeemer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AcademicsComponent } from './pages/academics/academics.component';
+import { EventsComponent } from './pages/events/events.component';
+import { FeedbackComponent } from './pages/feedback/feedback.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'academics', component: AcademicsComponent },
+    { path: 'feedback', component: FeedbackComponent},
+    { path: 'contact', component: ContactComponent},
+    { path: 'events', component: EventsComponent },
+    { path: '**', redirectTo: '' }
+];
