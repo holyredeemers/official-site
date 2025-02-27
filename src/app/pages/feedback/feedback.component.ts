36e7dbb92c57470dd0e68a831859505a097dfee3
Kingswin <kingswin5549@gmail.com>
2025-02-27 23:40:20 +0530

    Feedback Partially Done

--- src/app/pages/feedback/feedback.component.ts
+++ src/app/pages/feedback/feedback.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-feedback',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.css'
 })
 export class FeedbackComponent {
 
+  feedbacks = [
+    { name: "John Doe", year: "2010", occupation: "Software Engineer", comment: "This school shaped my career!" },
+    { name: "Jane Smith", year: "2015", occupation: "Doctor", comment: "The discipline and education here were top-notch!" },
+    { name: "Michael Lee", year: "2018", occupation: "Entrepreneur", comment: "A great place to learn and grow!" }
+  ];
+
+  currentIndex = 0;
+
+  nextSlide() {
+    if (this.currentIndex < this.feedbacks.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+  }
+
+  prevSlide() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.feedbacks.length - 1;
+    }
+  }
+
+  newFeedback = { name: '', year: '', occupation: '', comment: '' };
+
+  submitFeedback() {
+    if (this.newFeedback.name && this.newFeedback.year && this.newFeedback.occupation && this.newFeedback.comment) {
+      this.feedbacks.push({ ...this.newFeedback });
+      this.newFeedback = { name: '', year: '', occupation: '', comment: '' };
+    }
+  }
 }
